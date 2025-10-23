b72a8e99a296c2447320ece2ed25e18f5339cf41
Yatharth Bisht <vasub0723@gmail.com>
2025-10-23 18:04:07 +0530

    added incomplete question

--- week_5/ques3.java
+++ week_5/ques3.java
@@ -1,5 +1,24 @@
 package week_5;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class ques3 {
-    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        try{
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println("Division : " + a/b);
+        }
+        catch (ArithmeticException e){
+            System.err.println("Can't divide by zero ,error : " + e);
+        }
+        catch (InputMismatchException e){
+            System.err.println("Can't accept anything but int , error : "  + e );
+        }
+        finally{
+            System.out.println("finished execution setup");
+        }
+        in.close();
+    }
 }
